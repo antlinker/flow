e3427112ea8711f62252e7ed7981e311c88f9dab
lyric <tiannianshou@gmail.com>
2017-12-29 09:33:34 +0800

    update exec

--- exec.go
+++ exec.go
@@ -6,6 +6,9 @@ import (
 
 // Execer 表达式执行器
 type Execer interface {
+	// 执行表达式
+	Exec(ctx context.Context, exp, params []byte) (interface{}, error)
+
 	// 执行表达式返回布尔类型的值
 	ExecToBool(ctx context.Context, exp, params []byte) (bool, error)
 
